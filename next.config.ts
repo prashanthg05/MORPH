1f5d24fa7b64002d80f06f6bfb83caf5d517a149
prashanthg05 <prashanthg0505@gmail.com>
2026-03-04 11:39:40 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     unoptimized: true, // Crucial for Cloudflare Free Tier
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Prevents tiny code style errors from stopping deployment
   },
 };
 
