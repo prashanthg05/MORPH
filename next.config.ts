b759d892817aecab21f1bbcafb90cb57dead5d7e
prashanthg05 <prashanthg0505@gmail.com>
2026-03-07 18:23:26 +0530

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true, // Crucial for Cloudflare Free Tier
-  },
-};
-
-export default nextConfig;
